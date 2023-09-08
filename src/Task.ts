c507537e8722551249a8c383a507a1d1a37ad829
patricio henderson <patricio.henderson.v@gmail.com>
2023-09-08 13:46:51 -0300

    BaseSwapTask: Implement interface

--- src/Task.ts
+++ src/Task.ts
@@ -2,6 +2,7 @@ import { Address, Bytes, log } from '@graphprotocol/graph-ts'
 
 import {
   AcceptanceList,
+  BaseSwapTask,
   CustomTokenThreshold,
   CustomVolumeLimit,
   Task,
@@ -25,6 +26,9 @@ import {
   TxCostLimitPctSet,
   TxCostLimitSet,
 } from '../types/templates/Task/Task'
+import {
+  ConnectorSet
+} from '../types/templates/BaseSwap/BaseSwap'
 import { loadOrCreateERC20 } from './ERC20'
 
 export function handleBalanceConnectorsSet(event: BalanceConnectorsSet): void {
@@ -34,6 +38,75 @@ export function handleBalanceConnectorsSet(event: BalanceConnectorsSet): void {
   task.previousBalanceConnector = event.params.previous.toHexString()
   task.nextBalanceConnector = event.params.next.toHexString()
   task.save()
+}
+
+export function handleConnectorsSet(event: ConnectorSet): void {
+
+}
+
+export function handleCustomTokenThresholdSet(event: CustomTokenThresholdSet): void {
+  const task = Task.load(event.address.toHexString())
+  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
+
+  const customTokenThresholdId = getCustomTokenThresholdId(task, event.params.token)
+  let tokenThreshold = TokenThreshold.load(customTokenThresholdId)
+  if (tokenThreshold == null) tokenThreshold = new TokenThreshold(customTokenThresholdId)
+  tokenThreshold.token = loadOrCreateERC20(event.params.thresholdToken).id
+  tokenThreshold.min = event.params.min
+  tokenThreshold.max = event.params.max
+  tokenThreshold.save()
+
+  const customTokenThreshold = new CustomTokenThreshold(customTokenThresholdId)
+  customTokenThreshold.iTask = task.id
+  customTokenThreshold.token = loadOrCreateERC20(event.params.token).id
+  customTokenThreshold.threshold = tokenThreshold.id
+  customTokenThreshold.save()
+}
+
+export function handleCustomVolumeLimitSet(event: CustomVolumeLimitSet): void {
+  const task = Task.load(event.address.toHexString())
+  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
+
+  const customVolumeLimitId = getCustomVolumeLimitId(task, event.params.token)
+  let volumeLimit = VolumeLimit.load(customVolumeLimitId)
+  if (volumeLimit == null) volumeLimit = new VolumeLimit(customVolumeLimitId)
+  volumeLimit.token = loadOrCreateERC20(event.params.limitToken).id
+  volumeLimit.amount = event.params.amount
+  volumeLimit.period = event.params.period
+  volumeLimit.save()
+
+  let customVolumeLimit = CustomVolumeLimit.load(customVolumeLimitId)
+  if (customVolumeLimit == null) customVolumeLimit = new CustomVolumeLimit(customVolumeLimitId)
+  customVolumeLimit.iTask = task.id
+  customVolumeLimit.token = loadOrCreateERC20(event.params.token).id
+  customVolumeLimit.volumeLimit = volumeLimit.id
+  customVolumeLimit.save()
+}
+
+export function handleDefaultVolumeLimitSet(event: DefaultVolumeLimitSet): void {
+  const task = Task.load(event.address.toHexString())
+  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
+
+  const defaultVolumeLimitId = task.id
+  let defaultVolumeLimit = VolumeLimit.load(defaultVolumeLimitId)
+  if (defaultVolumeLimit == null) defaultVolumeLimit = new VolumeLimit(defaultVolumeLimitId)
+  defaultVolumeLimit.token = loadOrCreateERC20(event.params.token).id
+  defaultVolumeLimit.amount = event.params.amount
+  defaultVolumeLimit.period = event.params.period
+  defaultVolumeLimit.save()
+}
+
+export function handleDefaultTokenThresholdSet(event: DefaultTokenThresholdSet): void {
+  const task = Task.load(event.address.toHexString())
+  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
+
+  const defaultTokenThresholdId = task.id
+  let defaultTokenThreshold = TokenThreshold.load(defaultTokenThresholdId)
+  if (defaultTokenThreshold == null) defaultTokenThreshold = new TokenThreshold(defaultTokenThresholdId)
+  defaultTokenThreshold.token = loadOrCreateERC20(event.params.token).id
+  defaultTokenThreshold.min = event.params.min
+  defaultTokenThreshold.max = event.params.max
+  defaultTokenThreshold.save()
 }
 
 export function handleGasPriceLimitSet(event: GasPriceLimitSet): void {
@@ -49,48 +122,6 @@ export function handlePriorityFeeLimitSet(event: PriorityFeeLimitSet): void {
   if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
 
   task.priorityFeeLimit = event.params.priorityFeeLimit
-  task.save()
-}
-
-export function handleTokensAcceptanceTypeSet(event: TokensAcceptanceTypeSet): void {
-  const task = Task.load(event.address.toHexString())
-  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
-
-  const acceptanceListId = task.id
-  const acceptanceList = loadOrCreateAcceptanceList(acceptanceListId)
-  const acceptanceType = parseAcceptanceType(event.params.acceptanceType)
-  acceptanceList.type = acceptanceType
-  acceptanceList.save()
-}
-
-export function handleTokensAcceptanceListSet(event: TokensAcceptanceListSet): void {
-  const task = Task.load(event.address.toHexString())
-  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
-
-  const acceptanceListId = task.id
-  const acceptanceList = loadOrCreateAcceptanceList(acceptanceListId)
-  const tokens = acceptanceList.tokens
-  const token = loadOrCreateERC20(event.params.token).id
-  const index = tokens.indexOf(token)
-  if (token && index < 0) tokens.push(token)
-  else if (token && index >= 0) tokens.splice(index, 1)
-  acceptanceList.tokens = tokens
-  acceptanceList.save()
-}
-
-export function handleTxCostLimitPctSet(event: TxCostLimitPctSet): void {
-  const task = Task.load(event.address.toHexString())
-  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
-
-  task.txCostLimitPct = event.params.txCostLimitPct
-  task.save()
-}
-
-export function handleTxCostLimitSet(event: TxCostLimitSet): void {
-  const task = Task.load(event.address.toHexString())
-  if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
-
-  task.txCostLimit = event.params.txCostLimit
   task.save()
 }
 
@@ -118,69 +149,66 @@ export function handleTimeLockExpirationSet(event: TimeLockExpirationSet): void 
   task.save()
 }
 
-export function handleDefaultVolumeLimitSet(event: DefaultVolumeLimitSet): void {
+export function handleTokensAcceptanceListSet(event: TokensAcceptanceListSet): void {
   const task = Task.load(event.address.toHexString())
   if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
 
-  const defaultVolumeLimitId = task.id
-  let defaultVolumeLimit = VolumeLimit.load(defaultVolumeLimitId)
-  if (defaultVolumeLimit == null) defaultVolumeLimit = new VolumeLimit(defaultVolumeLimitId)
-  defaultVolumeLimit.token = loadOrCreateERC20(event.params.token).id
-  defaultVolumeLimit.amount = event.params.amount
-  defaultVolumeLimit.period = event.params.period
-  defaultVolumeLimit.save()
+  const acceptanceListId = task.id
+  const acceptanceList = loadOrCreateAcceptanceList(acceptanceListId)
+  const tokens = acceptanceList.tokens
+  const token = loadOrCreateERC20(event.params.token).id
+  const index = tokens.indexOf(token)
+  if (token && index < 0) tokens.push(token)
+  else if (token && index >= 0) tokens.splice(index, 1)
+  acceptanceList.tokens = tokens
+  acceptanceList.save()
 }
 
-export function handleCustomVolumeLimitSet(event: CustomVolumeLimitSet): void {
+export function handleTokensAcceptanceTypeSet(event: TokensAcceptanceTypeSet): void {
   const task = Task.load(event.address.toHexString())
   if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
 
-  const customVolumeLimitId = getCustomVolumeLimitId(task, event.params.token)
-  let volumeLimit = VolumeLimit.load(customVolumeLimitId)
-  if (volumeLimit == null) volumeLimit = new VolumeLimit(customVolumeLimitId)
-  volumeLimit.token = loadOrCreateERC20(event.params.limitToken).id
-  volumeLimit.amount = event.params.amount
-  volumeLimit.period = event.params.period
-  volumeLimit.save()
-
-  let customVolumeLimit = CustomVolumeLimit.load(customVolumeLimitId)
-  if (customVolumeLimit == null) customVolumeLimit = new CustomVolumeLimit(customVolumeLimitId)
-  customVolumeLimit.task = task.id
-  customVolumeLimit.token = loadOrCreateERC20(event.params.token).id
-  customVolumeLimit.volumeLimit = volumeLimit.id
-  customVolumeLimit.save()
+  const acceptanceListId = task.id
+  const acceptanceList = loadOrCreateAcceptanceList(acceptanceListId)
+  const acceptanceType = parseAcceptanceType(event.params.acceptanceType)
+  acceptanceList.type = acceptanceType
+  acceptanceList.save()
 }
 
-export function handleDefaultTokenThresholdSet(event: DefaultTokenThresholdSet): void {
+export function handleTxCostLimitPctSet(event: TxCostLimitPctSet): void {
   const task = Task.load(event.address.toHexString())
   if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
 
-  const defaultTokenThresholdId = task.id
-  let defaultTokenThreshold = TokenThreshold.load(defaultTokenThresholdId)
-  if (defaultTokenThreshold == null) defaultTokenThreshold = new TokenThreshold(defaultTokenThresholdId)
-  defaultTokenThreshold.token = loadOrCreateERC20(event.params.token).id
-  defaultTokenThreshold.min = event.params.min
-  defaultTokenThreshold.max = event.params.max
-  defaultTokenThreshold.save()
+  task.txCostLimitPct = event.params.txCostLimitPct
+  task.save()
 }
 
-export function handleCustomTokenThresholdSet(event: CustomTokenThresholdSet): void {
+export function handleTxCostLimitSet(event: TxCostLimitSet): void {
   const task = Task.load(event.address.toHexString())
   if (task == null) return log.warning('Missing task entity {}', [event.address.toHexString()])
 
-  const customTokenThresholdId = getCustomTokenThresholdId(task, event.params.token)
-  let tokenThreshold = TokenThreshold.load(customTokenThresholdId)
-  if (tokenThreshold == null) tokenThreshold = new TokenThreshold(customTokenThresholdId)
-  tokenThreshold.token = loadOrCreateERC20(event.params.thresholdToken).id
-  tokenThreshold.min = event.params.min
-  tokenThreshold.max = event.params.max
-  tokenThreshold.save()
+  task.txCostLimit = event.params.txCostLimit
+  task.save()
+}
 
-  const customTokenThreshold = new CustomTokenThreshold(customTokenThresholdId)
-  customTokenThreshold.task = task.id
-  customTokenThreshold.token = loadOrCreateERC20(event.params.token).id
-  customTokenThreshold.threshold = tokenThreshold.id
-  customTokenThreshold.save()
+export function getCustomVolumeLimitId(task: Task, token: Address): string {
+  return task.id.toString() + '/' + token.toHexString()
+}
+
+export function getCustomTokenThresholdId(task: Task, token: Address): string {
+  return task.id.toString() + '/' + token.toHexString()
+}
+
+export function getExecutionType(address: Address): Bytes {
+  const taskContract = TaskContract.bind(address)
+  const executionTypeCall = taskContract.try_EXECUTION_TYPE()
+
+  if (!executionTypeCall.reverted) {
+    return executionTypeCall.value
+  }
+
+  log.warning('EXECUTION_TYPE() call reverted for task {}', [address.toHexString()])
+  return Bytes.fromUTF8('')
 }
 
 export function getSmartVault(address: Address): Address {
@@ -207,40 +235,20 @@ export function getTokensSource(address: Address): Address {
   return Address.zero()
 }
 
-export function getExecutionType(address: Address): Bytes {
-  const taskContract = TaskContract.bind(address)
-  const executionTypeCall = taskContract.try_EXECUTION_TYPE()
-
-  if (!executionTypeCall.reverted) {
-    return executionTypeCall.value
-  }
-
-  log.warning('EXECUTION_TYPE() call reverted for task {}', [address.toHexString()])
-  return Bytes.fromUTF8('')
-}
-
-export function getCustomVolumeLimitId(task: Task, token: Address): string {
-  return task.id.toString() + '/' + token.toHexString()
-}
-
-export function getCustomTokenThresholdId(task: Task, token: Address): string {
-  return task.id.toString() + '/' + token.toHexString()
-}
-
-export function parseAcceptanceType(op: i32): string {
-  if (op == 0) return 'DenyList'
-  else return 'AllowList'
-}
-
 export function loadOrCreateAcceptanceList(tokensAcceptanceListId: string): AcceptanceList {
   let acceptanceList = AcceptanceList.load(tokensAcceptanceListId)
 
   if (acceptanceList === null) {
     acceptanceList = new AcceptanceList(tokensAcceptanceListId)
-    acceptanceList.task = tokensAcceptanceListId
+    acceptanceList.iTask = tokensAcceptanceListId
     acceptanceList.type = 'DenyList'
     acceptanceList.tokens = []
   }
 
   return acceptanceList
+}
+
+export function parseAcceptanceType(op: i32): string {
+  if (op == 0) return 'DenyList'
+  else return 'AllowList'
 }
