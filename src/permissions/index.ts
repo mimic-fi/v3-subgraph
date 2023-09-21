2b62e47e08dece13e8b8d7360b9d49183a63adf4
patricio henderson <patricio.henderson.v@gmail.com>
2023-09-21 20:31:12 -0300

    Index: Modify dictionary manipulation

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,6 +1,6 @@
 import dictionary from './dictionary'
 
 export function getFunctionNameForSelector(selector: string): string {
-  const name = dictionary[selector]
-  return name || 'Unknown'
+  const functionName = dictionary.has(selector) ? dictionary.get(selector) : 'Unknown'
+  return functionName
 }
