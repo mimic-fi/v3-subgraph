8b2873f8522df9e9ed30706e7acfa201680b1f47
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-11-12 18:07:18 +0300

    chore: setup studio deployments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,32 @@ set -o errexit
 # Run graph build
 yarn build:$NETWORK
 
-# Require $GRAPH_KEY to be set
-if [[ -z "${GRAPH_KEY}" ]]; then
-  echo "Please set \$GRAPH_KEY to your The Graph deploy key to run this command."
-  exit 1
+# Studio deployments
+platform_base=studio
+platform_zkevm=studio
+
+# Load platform
+if [[ -z $PLATFORM ]]; then
+  PLATFORM_VAR=platform_$NETWORK
+  PLATFORM=${!PLATFORM_VAR}
 fi
 
 # Deploy subgraph
-graph deploy mimic-fi/v3-$NETWORK --product hosted-service --access-token "$GRAPH_KEY"
+if [ "$PLATFORM" == "studio" ]; then
+    echo "Deploying $NETWORK to subgraph studio"
+    if [[ -z $VERSION_LABEL ]]; then
+      echo 'Please make sure a version label is provided'
+      exit 1
+    fi
+    yarn graph deploy mimic-v3-$NETWORK --studio --deploy-key $GRAPH_KEY -l $VERSION_LABEL
+else
+    echo "Deploying $NETWORK to hosted service"
+    yarn graph deploy mimic-fi/v3-$NETWORK --product hosted-service --deploy-key $GRAPH_KEY
+fi
+
+# Check deployer status
+if [ $? -ne 0 ]; then
+    echo "Error trying to deploy subgraph with exit status $?"
+    echo "$output"
+    exit $?
+fi
