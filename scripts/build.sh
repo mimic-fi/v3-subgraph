406d75611c7407757b49d7eed4980a500500689b
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-02-15 07:00:02 -0300

    relayer: add v2 data source

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,77 +6,88 @@ set -o errexit
 # Arbitrum
 registry_arbitrum=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_arbitrum=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_arbitrum=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_arbitrum=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_arbitrum=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_arbitrum=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_arbitrum=117042327
 
 # Aurora
 registry_aurora=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_aurora=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_aurora=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_aurora=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_aurora=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_aurora=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_aurora=104855032
 
 # Avalanche
 registry_avalanche=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_avalanche=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_avalanche=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_avalanche=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_avalanche=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_avalanche=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_avalanche=37130982
 
 # Base
 registry_base=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_base=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_base=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_base=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_base=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_base=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_base=5957564
 
 # BSC
 registry_bsc=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_bsc=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_bsc=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_bsc=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_bsc=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_bsc=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_bsc=33066995
 
 # Fantom
 registry_fantom=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_fantom=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_fantom=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_fantom=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_fantom=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_fantom=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_fantom=69985942
 
 # Gnosis
 registry_gnosis=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_gnosis=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_gnosis=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_gnosis=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_gnosis=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_gnosis=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_gnosis=30715174
 
 # Mainnet
 registry_mainnet=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_mainnet=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_mainnet=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_mainnet=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_mainnet=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_mainnet=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_mainnet=18386855
 
 # Optimism
 registry_optimism=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_optimism=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_optimism=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_optimism=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_optimism=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_optimism=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_optimism=111552683
 
 # Polygon
 registry_polygon=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_polygon=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_polygon=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_polygon=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_polygon=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_polygon=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_polygon=49350048
 
 # zkEVM
 registry_zkevm=0x1675BF3F75046aCd131caD845eb8FF3Bed49a643
 deployer_zkevm=0x849B7B1102B0dcf6eC10f98b81C8D1c38f7cbf24
-relayer_zkevm=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_1_zkevm=0xD7252C026c3cA28D73B4DeeF62FE6ADe86eC17A9
+relayer_2_zkevm=0x54FC6E302043aAF56154e8B4A7F01645eDAdA906
 feeController_zkevm=0x88586bfc840b99680c8cc753a36b51999608b1f6
 block_zkevm=6788022
 
@@ -133,25 +144,37 @@ if [[ -z $DEPLOYER_ADDRESS ]]; then
   exit 1
 fi
 
-# Load relayer address
-if [[ -z $RELAYER_ADDRESS ]]; then
-  RELAYER_ADDRESS_VAR=relayer_$NETWORK
-  RELAYER_ADDRESS=${!RELAYER_ADDRESS_VAR}
+# Load relayer 1 address
+if [[ -z $RELAYER_1_ADDRESS ]]; then
+  RELAYER_1_ADDRESS_VAR=relayer_1_$NETWORK
+  RELAYER_1_ADDRESS=${!RELAYER_1_ADDRESS_VAR}
 fi
 
-# Validate relayer address
-if [[ -z $RELAYER_ADDRESS ]]; then
-  echo 'Please make sure a Relayer address is provided'
+# Validate relayer 1 address
+if [[ -z $RELAYER_1_ADDRESS ]]; then
+  echo 'Please make sure a Relayer 1 address is provided'
   exit 1
 fi
 
-# Load feeController address
+# Load relayer 2 address
+if [[ -z $RELAYER_2_ADDRESS ]]; then
+  RELAYER_2_ADDRESS_VAR=relayer_2_$NETWORK
+  RELAYER_2_ADDRESS=${!RELAYER_2_ADDRESS_VAR}
+fi
+
+# Validate relayer 2 address
+if [[ -z $RELAYER_2_ADDRESS ]]; then
+  echo 'Please make sure a Relayer 2 address is provided'
+  exit 1
+fi
+
+# Load fee controller address
 if [[ -z $FEE_CONTROLLER_ADDRESS ]]; then
   FEE_CONTROLLER_ADDRESS_VAR=feeController_$NETWORK
   FEE_CONTROLLER_ADDRESS=${!FEE_CONTROLLER_ADDRESS_VAR}
 fi
 
-# Validate relayer address
+# Validate fee controller address
 if [[ -z $FEE_CONTROLLER_ADDRESS ]]; then
   echo 'Please make sure a Fee Controller address is provided'
   exit 1
@@ -173,7 +196,8 @@ cp subgraph.template.yaml subgraph.yaml
 sed -i -e "s/{{network}}/${ENV}/g" subgraph.yaml
 sed -i -e "s/{{registryAddress}}/${REGISTRY_ADDRESS}/g" subgraph.yaml
 sed -i -e "s/{{deployerAddress}}/${DEPLOYER_ADDRESS}/g" subgraph.yaml
-sed -i -e "s/{{relayerAddress}}/${RELAYER_ADDRESS}/g" subgraph.yaml
+sed -i -e "s/{{relayer1Address}}/${RELAYER_1_ADDRESS}/g" subgraph.yaml
+sed -i -e "s/{{relayer2Address}}/${RELAYER_2_ADDRESS}/g" subgraph.yaml
 sed -i -e "s/{{feeControllerAddress}}/${FEE_CONTROLLER_ADDRESS}/g" subgraph.yaml
 sed -i -e "s/{{blockNumber}}/${BLOCK_NUMBER}/g" subgraph.yaml
 rm -f subgraph.yaml-e
