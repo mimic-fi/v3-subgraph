9c61b1fdb2cfd94c6daec5413994516c33a7c51e
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-05-13 13:06:16 -0300

    chore: update deployment script to use studio only

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,32 +6,31 @@ set -o errexit
 # Run graph build
 yarn build:$NETWORK
 
-# Studio deployments
-platform_base=studio
-platform_zkevm=studio
-
 # Load platform
-if [[ -z $PLATFORM ]]; then
-  PLATFORM_VAR=platform_$NETWORK
-  PLATFORM=${!PLATFORM_VAR}
-fi
-
-# Deploy subgraph
-if [ "$PLATFORM" == "studio" ]; then
-    echo "Deploying $NETWORK to subgraph studio"
-    if [[ -z $VERSION_LABEL ]]; then
-      echo 'Please make sure a version label is provided'
-      exit 1
-    fi
-    yarn graph deploy mimic-v3-$NETWORK --studio --deploy-key $GRAPH_KEY -l $VERSION_LABEL
+if [[ "$NETWORK" = "aurora" || "$NETWORK" = "bsc" ]]; then
+  yarn graph deploy mimic-fi/v3-$NETWORK --product hosted-service --deploy-key $GRAPH_KEY
 else
-    echo "Deploying $NETWORK to hosted service"
-    yarn graph deploy mimic-fi/v3-$NETWORK --product hosted-service --deploy-key $GRAPH_KEY
+  # Check deployment version
+  if [[ -z $VERSION_LABEL ]]; then
+    echo 'Please make sure a version label is provided'
+    exit 1
+  fi
+
+  # Define subgraph name
+  if [[ "$NETWORK" = "base" || "$NETWORK" = "zkevm" ]]; then
+    NAME=mimic-v3-$NETWORK
+  else
+    NAME=v3-$NETWORK
+  fi
+
+  # Deploy subgraph
+  echo "Deploying $NAME to subgraph studio"
+  yarn graph deploy mimic-v3-$NETWORK --studio --deploy-key $GRAPH_KEY -l $VERSION_LABEL
 fi
 
 # Check deployer status
 if [ $? -ne 0 ]; then
-    echo "Error trying to deploy subgraph with exit status $?"
-    echo "$output"
-    exit $?
+  echo "Error trying to deploy subgraph with exit status $?"
+  echo "$output"
+  exit $?
 fi
