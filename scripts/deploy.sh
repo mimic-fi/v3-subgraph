1806b4bc912fb26caf55b8e1f302b20eceeaac75
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2024-06-13 17:24:57 -0300

    chore: fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,27 +6,22 @@ set -o errexit
 # Run graph build
 yarn build:$NETWORK
 
-# Load platform
-if [[ "$NETWORK" = "aurora" || "$NETWORK" = "bsc" ]]; then
-  yarn graph deploy mimic-fi/v3-$NETWORK --product hosted-service --deploy-key $GRAPH_KEY
-else
-  # Check deployment version
-  if [[ -z $VERSION_LABEL ]]; then
-    echo 'Please make sure a version label is provided'
-    exit 1
-  fi
-
-  # Define subgraph name
-  if [[ "$NETWORK" = "base" || "$NETWORK" = "zkevm" ]]; then
-    NAME=mimic-v3-$NETWORK
-  else
-    NAME=v3-$NETWORK
-  fi
-
-  # Deploy subgraph
-  echo "Deploying $NAME to subgraph studio"
-  yarn graph deploy mimic-v3-$NETWORK --studio --deploy-key $GRAPH_KEY -l $VERSION_LABEL
+# Check deployment version
+if [[ -z $VERSION_LABEL ]]; then
+  echo 'Please make sure a version label is provided'
+  exit 1
 fi
+
+# Define subgraph name
+if [[ "$NETWORK" = "base" || "$NETWORK" = "zkevm" ]]; then
+  NAME=mimic-v3-$NETWORK
+else
+  NAME=v3-$NETWORK
+fi
+
+# Deploy subgraph
+echo "Deploying $NAME to subgraph studio"
+yarn graph deploy $NAME --studio --deploy-key $GRAPH_KEY -l $VERSION_LABEL
 
 # Check deployer status
 if [ $? -ne 0 ]; then
